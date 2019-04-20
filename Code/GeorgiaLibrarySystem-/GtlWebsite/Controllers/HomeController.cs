acd440fdc6953865568735e0bb7dd96f5431514a
RaidenRabit <1062021@ucn.dk>
2019-04-20 12:38:55 +0200

    #25 initial commit with linked projects

---
+++ Code/GeorgiaLibrarySystem-/GtlWebsite/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using GtlWebsite.ServiceReference1;
+
+namespace GtlWebsite.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string ContactService(int a)
+        {
+            Service1Client b = new Service1Client();
+            return b.GetData(a);
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
