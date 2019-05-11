1f24feb1bb934e69f30ec2c5cde15a155c01402e
Ralf <bubriks@gmail.com>
2019-05-11 20:23:21 +0200

    #40 Comments addressed

--- Code/GeorgiaLibrarySystem-/GtlService/DataAccess/ExampleClass.cs
+++ Code/GeorgiaLibrarySystem-/GtlService/DataAccess/ExampleClass.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using GtlService.Model;
 
-namespace GtlService.DataManagement
+namespace GtlService.DataAccess
 {
     class TestClass
     {
