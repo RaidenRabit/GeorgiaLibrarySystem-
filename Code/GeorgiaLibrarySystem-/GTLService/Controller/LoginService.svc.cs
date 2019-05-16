66d5f965195c948865afc0fe99aeec5d215dd1a3
Ralf <bubriks@gmail.com>
2019-05-16 11:08:58 +0200

    #41 Service with parameters

--- Code/GeorgiaLibrarySystem-/GTLService/Controller/LoginService.svc.cs
+++ Code/GeorgiaLibrarySystem-/GTLService/Controller/LoginService.svc.cs
@@ -1,28 +1,20 @@
-﻿using Core;
-using GtlService.DataAccess.Database;
-using GtlService.DataManagement.Database;
+﻿using System.ServiceModel;
 using GtlService.DataManagement.iDataManagement;
 using GTLService.Controller.IController;
 
 namespace GTLService.Controller
 {
+    [ServiceBehavior(IncludeExceptionDetailInFaults = true)]
     public class LoginService : ILoginService
     {
         private ILoginDm _loginDm;
-        public LoginService()
-        {
-            _loginDm = new LoginDm_Database(new LoginDa_Database(new Context()));
-        }
         public LoginService(ILoginDm loginDm)
         {
-            if(loginDm != null)
-                _loginDm = loginDm;
+            _loginDm = loginDm;
         }
-
-        //todo fix here!
+        
         public bool Login(int ssn, string password)
         {
-            //ILoginDm loginDm = new LoginDmFactory().Get(id);
             return _loginDm.Login(ssn, password);
         }
     }
---
+++ Code/GeorgiaLibrarySystem-/GTLService/WindsorInstaller.cs
@@ -0,0 +1,53 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using Core;
+using GtlService.DataAccess.Database;
+using GtlService.DataAccess.IDataAccess;
+using GtlService.DataManagement.Database;
+using GtlService.DataManagement.iDataManagement;
+using GTLService.Controller;
+using GTLService.Controller.IController;
+
+namespace GTLService
+{
+    public class WindsorInstaller : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            //used: http://scotthannen.org/blog/2016/04/13/wcf-dependency-injection-in-5-minutes.html
+            container.Register(
+                Component.For<ILoginService, LoginService>(),
+                Component.For<ILoginDm, LoginDm_Database>(),
+                Component.For<ILoginDa, LoginDa_Database>(),
+                Component.For<Context, Context>());
+        }
+    }
+}
+
+/*
+ * using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using GtlService.DataAccess.Database;
+using GtlService.DataAccess.IDataAccess;
+using GtlService.DataManagement.Database;
+using GtlService.DataManagement.iDataManagement;
+using GTLService.Controller.IController;
+using Unity;
+using Unity.Wcf;
+
+namespace GTLService
+{
+    public class WcfServiceFactory: UnityServiceHostFactory
+    {
+        protected override void ConfigureContainer(IUnityContainer container)
+        {
+            container.RegisterType<ILoginDm,LoginDm_Database>();
+            container.RegisterType<ILoginDa,LoginDa_Database>();
+            container.RegisterType<ILoginService,ILoginService>();
+        }
+    }
+}
+ */
