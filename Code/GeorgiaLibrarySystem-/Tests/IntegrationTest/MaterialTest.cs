f8b2c592a67922123f3a2bbc331f5a311a6f88ab
RaidenRabit <abirta05@yahoo.com>
2019-05-19 13:10:04 +0200

    #33 integration tests

--- Code/GeorgiaLibrarySystem-/Tests/IntegrationTest/MaterialTest.cs
+++ Code/GeorgiaLibrarySystem-/Tests/IntegrationTest/MaterialTest.cs
@@ -1,6 +1,13 @@
-﻿using System;
-using System.Data.SqlClient;
+﻿using System.Data.SqlClient;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using Core;
+using GTLService.Controller;
+using GTLService.DataAccess.Code;
+using GTLService.DataAccess.Database;
+using GTLService.DataManagement.Code;
+using GTLService.DataManagement.Database;
 using Microsoft.SqlServer.Management.Common;
 using Microsoft.SqlServer.Management.Smo;
 using NUnit.Framework;
@@ -10,66 +17,154 @@ namespace Tests.IntegrationTest
 
     public class MaterialTest
     {
-        public void GetMaterialsCode(string materialTitle, string author, int numOfRecords = 10, int isbn = 0, string jobStatus = "0")
-        {
 
-        }
-
-        public void CreateMaterialCode(int ssn, int isbn, string library, string author, string description, string title, string typeName,
-            int quantity)
-        {
-
-        }
-
-        public void DeleteMaterialCode(int ssn, int isbn)
-        {
-
-        }
-
-
-        public void DeleteCopyCode(int ssn, int copyId)
-        {
-
-        }
+        private IMaterialService _materialService;
         
-        public void GetMaterialsDb(string materialTitle, string author, int numOfRecords = 10, int isbn = 0, string jobStatus = "0")
+        [Test]
+        //Code approach
+        [TestCase("", "", null, null, "", "Code", 10)]
+        [TestCase("horror book", "", null, null, "", "Code", 1)]
+        [TestCase("horror book", "hala", null, null, "", "Code", 5)]
+        [TestCase("", "", 6, null, "", "Code", 6)]
+        [TestCase("", "", null, 1, "", "Code", 4)]
+        [TestCase("", "", null, null, "book", "Code", 7)]
+        //Database approach
+        [TestCase("", "", null, null, "", "Database", 10)]
+        [TestCase("horror book", "", null, null, "", "Database", 1)]
+        [TestCase("horror book", "hala", null, null, "", "Database", 5)]
+        [TestCase("", "", 6, null, "", "Database", 6)]
+        [TestCase("", "", null, 1, "", "Database", 4)]
+        [TestCase("", "", null, null, "book", "Database", 7)]
+        public void GetMaterials(string materialTitle, string author, int numOfRecords, int isbn, string jobStatus,
+            string approach, int resultCount)
         {
+            //Arrange
+            Setup(approach);
+            
+            //Act
+            var result = _materialService.GetMaterials(materialTitle, author, numOfRecords, isbn, jobStatus);
 
-        }
-
-        public void CreateMaterialDb(int ssn, int isbn, string library, string author, string description, string title, string typeName,
-            int quantity)
-        {
-
-        }
-
-        public void DeleteMaterialDb(int ssn, int isbn)
-        {
+            //Assert
+            Assert.IsTrue(result.Count.Equals(resultCount));
 
         }
 
         [Test]
-        public void DeleteCopyDb(int ssn, int copyId)
+        //Code approach
+        [TestCase(123456785, 1, "GTL", "books", "Code", true)]
+        [TestCase(123456785, 0, "GTL", "books", "Code", true)]    
+        [TestCase(123456785, 0, "Non-existent", "Non-existent", "Code", false)]
+        [TestCase(0, 0, "GTL", "books", "Code", false)]   
+        [TestCase(0, 1, "Non-existent", "books", "Code", false)]    
+        [TestCase(0, 1, "GTL", "books", "Code", false)]  
+        //Database approach
+        [TestCase(123456785, 1, "GTL", "books", "Database", true)]
+        [TestCase(123456785, 0, "GTL", "books", "Database", true)]    
+        [TestCase(123456785, 0, "Non-existent", "Non-existent", "Database", false)]
+        [TestCase(0, 0, "GTL", "books", "Database", false)]   
+        [TestCase(0, 1, "Non-existent", "books", "Database", false)]    
+        [TestCase(0, 1, "GTL", "books", "Database", false)]  
+        public void CreateMaterial(int ssn, int isbn, string library, string typeName, string approach, bool passing)
         {
-            Assert.IsTrue(true);
+            //Arrange
+            Setup(approach);
+            string author = "TestAuthor", description = "TestDescription", title = "testTitle";
+            int quantity = 2;
+
+            //Act
+            bool result =
+                _materialService.CreateMaterial(ssn, isbn, library, author, description, title, typeName, quantity);
+
+            //Assert
+            Assert.IsTrue(result.Equals(passing));
+        }
+        
+        [Test]
+        //Code approach
+        [TestCase(0,0, "Code", false)] //invalid ssn and isbn
+        [TestCase(123456785,0, "Code", false)] //valid ssn, invalid isbn
+        [TestCase(0,1, "Code", false)] //invalid ssn, valid isbn
+        [TestCase(123456785,1, "Code", true)] //valid ssn and isbn
+        //Database approach
+        [TestCase(0,0, "Database", false)] //invalid ssn and isbn
+        [TestCase(123456785,0, "Database", false)] //valid ssn, invalid isbn
+        [TestCase(0,1, "Database", false)] //invalid ssn, valid isbn
+        [TestCase(123456785,1, "Database", true)] //valid ssn and isbn
+        public void DeleteMaterial(int ssn, int isbn, string approach, bool passing)
+        {
+            //Arrange
+            Setup(approach);
+
+            //Act
+            bool result = _materialService.DeleteMaterial(ssn, isbn);
+
+            //Assert
+            Assert.IsTrue(result.Equals(passing));
         }
 
-        [SetUp]
-        public void Setup()
+        [Test]
+        //Code approach
+        [TestCase(0,0, "Code", false)] //invalid ssn and id
+        [TestCase(123456785,0, "Code", false)] //valid ssn, invalid id
+        [TestCase(0,1, "Code", false)] //invalid ssn, valid id
+        [TestCase(123456785,1, "Code", true)] //valid ssn and id
+        //Db approach
+        [TestCase(0,0, "Database", false)] //invalid ssn and id
+        [TestCase(123456785,0, "Database", false)] //valid ssn, invalid id
+        [TestCase(0,1, "Database", false)] //invalid ssn, valid id
+        [TestCase(123456785,1, "Database", true)] //valid ssn and id
+        public void DeleteCopy(int ssn, int copyId, string approach, bool passing)
+        {
+            //Arrange
+            Setup(approach);
+
+            //Act
+            bool result = _materialService.DeleteCopy(ssn, copyId);
+
+            //Assert
+            Assert.IsTrue(result.Equals(passing));
+        }
+        
+        private void Setup(string approach)
+        {
+            ResetDatabase();
+            Context context = new Context();
+            switch (approach)
+            {
+                case "Code":
+                    MaterialDa_Code materialDa_Code = new MaterialDa_Code(context);
+                    LibraryDa_Code libraryDa_Code = new LibraryDa_Code(context);
+                    PersonDa_Code personDa_Code = new PersonDa_Code(context);
+                    MaterialDm_Code materialsDm_Code = new MaterialDm_Code(materialDa_Code, libraryDa_Code, personDa_Code);
+                    _materialService = new MaterialService(materialsDm_Code);
+                    break;
+                case "Database":
+                    MaterialsDa_Database materialDa_Db = new MaterialsDa_Database(context);
+                    MaterialsDm_Database materialsDm_Db = new MaterialsDm_Database(materialDa_Db);
+                    _materialService = new MaterialService(materialsDm_Db);
+                    break;
+            }
+        }
+        
+        private void ResetDatabase()
         {
             string connectionString =
-                "metadata=res://*/GTLModel.csdl|res://*/GTLModel.ssdl|res://*/GTLModel.msl;provider=System.Data.SqlClient;provider connection string=&quot;data source=localhost;initial catalog=GTL;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework&quot;";
+                "data source=localhost;initial catalog=GTL;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
             
-            string currentDir = Environment.CurrentDirectory;
-            DirectoryInfo directory = new DirectoryInfo(
-                Path.GetFullPath(Path.Combine(currentDir, @"..\..\" + "SQLCreateQuery.sql")));
-            string path = directory.ToString();
+            var assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith("SQLCreateQuery.sql"));
 
-            string script = File.ReadAllText(@"C:\Users\abirt\source\repos\RaidenRabit\GeorgiaLibrarySystem-\Code\Database\SQLCreateQuery.sql");
-            SqlConnection conn = new SqlConnection(connectionString);
-            Server server = new Server(new ServerConnection(conn));
-            server.ConnectionContext.ExecuteNonQuery(script);
-            conn.Close();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+
+                SqlConnection conn = new SqlConnection(connectionString);
+                Server server = new Server(new ServerConnection(conn));
+                server.ConnectionContext.ExecuteNonQuery(result);
+                conn.Close();
+            }
         }
     }
 }
